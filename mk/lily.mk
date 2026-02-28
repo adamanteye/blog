96404efddad1ec64d1b5a103a88d6fe4eed0b6f7
Xuelin Yang <xuelin@adamanteye.cc>
2026-02-28 18:16:07 +0800

    feat(2026/02/28-south-sea-route): add reference

--- mk/lily.mk
+++ mk/lily.mk
@@ -1,4 +1,4 @@
-LILY_OBJS := $(patsubst src/%,build/%,$(patsubst %.ly,%.ly.svg,$(shell find src/ -name '*.ly')))
+LILY_OBJS := $(patsubst %.ly,%.ly.svg,$(shell find src/ -name '*.ly'))
 
 ifeq ($(V),1)
 	LILY := lilypond
@@ -8,11 +8,9 @@ else
 	LILY_S := 2>/dev/null
 endif
 
-build/%.ly.svg: src/%.ly
+%.ly.svg: %.ly
 	$(call log,LILY,$@)
-	$(Q)$(MKDIR_P) $(@D)
-	$(Q)$(CP_R) $< $(@D)
-	$(Q)$(LILY) -o $(@D)/$(<F) --svg $(@D)/$(<F) $(LILY_S)
+	$(Q)$(LILY) -o $< --svg $< $(LILY_S)
 ifeq ($(MINIFY), y)
 	$(call log,MINI,$@)
 	$(Q)$(MINIFY_CMD) -a -i $@
--- src/2026/02/28-south-sea-route/index.typ
+++ src/2026/02/28-south-sea-route/index.typ
@@ -2,4 +2,10 @@
 #import "meta.typ": desc, title
 #show: conf.with(title: title, desc: desc)
 
-#figure(image("南洋航路.ly.svg"))
+
+#figure(link("./南洋航路.ly", image("南洋航路.ly.svg")))
+
+/ References:
+
+- #link("https://musescore.com/user/35709044/scores/8057328")[ラバウル小唄
+    南洋航路 – 若杉雄三郎]
