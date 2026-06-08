f44ad0d33faad32cc4c0fdd0056ec3fe8b8ce754
Xuelin Yang <xuelin@adamanteye.cc>
2026-06-08 08:56:48 +0800

    make: fix lilypond production

--- mk/lily.mk
+++ mk/lily.mk
@@ -1,16 +1,21 @@
-LILY_OBJS := $(patsubst %.ly,%.ly.svg,$(shell find src/ -name '*.ly'))
-
 ifeq ($(V),y)
-	LILY := lilypond
+	LILYPOND := lilypond
 	LILY_S :=
 else
-	LILY := lilypond -s
+	LILYPOND := lilypond -s
 	LILY_S := 2>/dev/null
 endif
 
-%.ly.svg: %.ly
+LILY_SRCS := $(shell find src/ -name '*.ly')
+LILY_OBJS := $(patsubst src/%.ly,build/%.ly.svg,$(LILY_SRCS))
+
+.NOTINTERMEDIATE: $(LILY_OBJS)
+
+
+build/%.ly.svg: src/%.ly
 	$(call log,LILY,$@)
-	$(Q)$(LILY) -o $< --svg $< $(LILY_S)
+	$(Q)$(MKDIR_P) "$(@D)"
+	$(Q)$(LILYPOND) -o "$(@:.svg=)" --svg "$<" $(LILY_S)
 ifeq ($(MINIFY), y)
 	$(call log,MINI,$@)
 	$(Q)$(MINIFY_CMD) -a -i $@
